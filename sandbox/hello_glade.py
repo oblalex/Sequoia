060e3e047b69f1979a737aafc826ca1030517494
Alexander Oblovatniy <oblovatniy@gmail.com>
2013-09-19 21:23:33 +0300

    gtk glade example was added

---
+++ sandbox/hello_glade.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pygtk
+pygtk.require('2.0')
+import gtk
+import gtk.glade
+
+
+class HelloWorldGlade(object):
+    """This is an Hello World GTK application"""
+
+    def __init__(self):
+        gladefile = "glade/hello_glade.glade"
+        self.wTree = gtk.Builder()
+        self.wTree.add_from_file(gladefile)
+
+        self.window = self.wTree.get_object('main_window')
+        if (self.window):
+            dic = {
+                'on_togglebutton_toggled' : self.on_togglebutton_toggled,
+                'on_main_window_delete_event': self.delete_event,
+                'on_checkbox_toggled': self.on_checkbox_toggled,
+            }
+            self.wTree.connect_signals(dic)
+            self.window.show()
+
+    def on_togglebutton_toggled(self, widget, data=None):
+        print "Toggle button is", widget.get_active()
+
+    def on_checkbox_toggled(self, widget, data=None):
+        print "Checkbox is", widget.get_active()
+
+    def delete_event(self, widget, event, data=None):
+        gtk.main_quit()
+        return False
+
+
+if __name__ == "__main__":
+    wnd = HelloWorldGlade()
+    gtk.main()
