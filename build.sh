e3d6e111cb0e885a8ae82ce3542344e367a1e21d
Alexander Oblovatniy <oblovatniy@gmail.com>
2013-10-06 10:03:35 +0300

    build script updated

--- build.sh
+++ build.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-CDIR=`pwd`
-wget --no-check-certificate http://freenet.mcnabhosting.com/python/pySpeex/pySpeex.tar.gz -O /tmp/pySpeex.src.tar.gz
+apt-get install python2.7-dev python-pip python-gtk2 libspeex-dev libportaudio2
+
+wget http://people.csail.mit.edu/hubert/pyaudio/packages/python-pyaudio_0.2.7-1_i386.deb -O /tmp/python-pyaudio.deb
+dpkg -i /tmp/python-pyaudio.deb
+
+wget http://freenet.mcnabhosting.com/python/pySpeex/pySpeex.tar.gz -O /tmp/pySpeex.src.tar.gz
 mkdir /tmp/pySpeex.src
 tar -xvf /tmp/pySpeex.src.tar.gz -C /tmp/pySpeex.src
 cd /tmp/pySpeex.src/python
 sed -i 's/speex.h/speex\/speex.h/' speex.c
 python setup.py build
 python setup.py install
-cd $CDIR"/sequoia/gost"
-make && make clean
-cd $CDIR
+cd -
 
+cd sequoia/gost
+make && make clean
+cd -
